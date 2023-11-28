e9b0bf1fd6b50a8488c528837b04bc9734904c25
gazabb-code <mo.amir.code@gmail.com>
2023-11-28 18:41:55 +0530

    allowed credentials

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
