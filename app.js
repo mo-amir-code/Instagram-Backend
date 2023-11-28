b2dbf5bc8528d1f6d5c5388494fae91c38edc67b
gazabb-code <mo.amir.code@gmail.com>
2023-11-28 18:27:23 +0530

    allow origin

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const cors = require("cors");
 const { connectToDB } = require("./services/Database");
 const router = require("./routers");
 
-
 const app = express();
 app.use(cors());
-app.use(express.json({limit: "25mb"}));
-app.use(express.urlencoded({limit: "25mb", extended: true}));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+});
 
 app.use(router);
 
-connectToDB().catch((err)=>{
-    console.log(err.message, "Database connection error");
-})
-
-
+connectToDB().catch((err) => {
+  console.log(err.message, "Database connection error");
+});
 
 module.exports = app;
