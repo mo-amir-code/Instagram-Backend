916142cc011eaefede6011bc9108b5ba0cfb979f
Mo Amir <130815672+mo-amir-code@users.noreply.github.com>
2024-03-24 02:54:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://instagram-frontend-moamir.netlify.app");
+  res.header("Access-Control-Allow-Origin", "https://instagram-fullstack-amir.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", true);
