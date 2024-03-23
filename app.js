3cc8f08f7987caccc97013345ef1752ba6a4e517
Mo Amir <mo.amir.code@gmail.com>
2024-03-24 00:38:49 +0530

    update app.js and add vercel.json

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header(
     "Access-Control-Allow-Origin",
-    "https://instagram-frontend-moamir.netlify.app"
+    "https://instagram-fullstack-amir.vercel.app"
   );
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
