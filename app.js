b63b7ea20329f5533503d8185d50acc0f35739c7
Mo Amir <130815672+mo-amir-code@users.noreply.github.com>
2024-03-24 01:34:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const { connectToDB } = require("./services/Database");
 const router = require("./routers");
 
 const app = express();
-app.use(cors());
+// Use the cors middleware to enable CORS
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://instagram-fullstack-amir.vercel.app"
+  ],
+  methods: ["GET", "PUT", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true
+}));
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173",
-    "https://instagram-fullstack-amir.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
+
 
 app.use(router);
 
