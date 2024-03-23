fdff2ff1d8e70b0f0a8fe1fc060ffbd104f482f0
Mo Amir <130815672+mo-amir-code@users.noreply.github.com>
2024-03-24 03:01:07 +0530

    Update app.js with comment

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://instagram-fullstack-amir.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Allow-Origin", "https://instagram-fullstack-amir.vercel.app");
+  // res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  // res.header("Access-Control-Allow-Headers", "Content-Type");
+  // res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
