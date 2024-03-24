2aa1fe2c69ef94dd3e4cc76f1afb3f8aaefcd10b
Mo Amir <mo.amir.code@gmail.com>
2024-03-25 00:10:33 +0530

    cors for socket

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { uploadOnCloudinaryForChat } = require("./services/UploadCloudinary");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["https://instagram-fullstack-amir.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials:true,
   },
