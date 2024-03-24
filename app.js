ee23dd9a7c58d72495e55f1e02fc10630e0940a3
Mo Amir <mo.amir.code@gmail.com>
2024-03-24 22:56:34 +0530

    allowed all origin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.SOCKET_ORIGIN);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", true);
