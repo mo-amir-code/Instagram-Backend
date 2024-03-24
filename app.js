ada87d3b088e163febcb67e2e4c9a38c837c30ff
Mo Amir <mo.amir.code@gmail.com>
2024-03-25 00:13:09 +0530

    sss cors update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', ["https://instagram-fullstack-amir.vercel.app", "http://localhost:5173"]);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
   res.setHeader('Access-Control-Allow-Credentials', true);
